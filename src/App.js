6bcbd347a244dda7bebc5437281bcabef0bdd677
Dawit <dawit@ginetta.net>
2019-09-02 16:09:36 +0200

    Add destructured array with two variables for the useState to update, introduce onClick events for which other callback functions are defined

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
+	const [ listMovies, setListMovies ] = useState('');
+	const [ listCharacters, setListCharacters ] = useState('');
+
+	const handleShowMovies = (e) => {
+		setListMovies('e.target.value');
+	};
+	const handleShowCharacters = (e) => {
+		setListCharacters('e.target.value');
+	};
 	return (
 		<div className="App" style={{ display: 'flex', flexDirection: 'row' }}>
 			<div style={{ display: 'flex', flexDirection: 'column', marginTop: '50px' }}>
@@ -15,12 +25,12 @@ function App() {
 						border: '1px solid darkgrey'
 					}}
 				/>
-				<button>Movies</button>
+				<button onClick={handleShowMovies}>Movies</button>
 
-				<ul>
-					<li>First</li>
-					<li>Second</li>
-					<li>Third</li>
+				<ul value={listMovies}>
+					<li>First Movie</li>
+					<li>Second Movie</li>
+					<li>Third Movie</li>
 				</ul>
 			</div>
 			<div style={{ display: 'flex', flexDirection: 'column', marginTop: '50px' }}>
@@ -33,11 +43,11 @@ function App() {
 						border: '1px solid darkgrey'
 					}}
 				/>
-				<button>Characters in the Movies</button>
-				<ul>
-					<li>First</li>
-					<li>Second</li>
-					<li>Third</li>
+				<button onClick={handleShowCharacters}>Characters in the Movies</button>
+				<ul value={listCharacters}>
+					<li>Character one</li>
+					<li>Character two</li>
+					<li>Character three</li>
 				</ul>
 			</div>
 		</div>
