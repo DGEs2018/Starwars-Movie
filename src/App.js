38734745ed83418744031134cf90b8d3bce366a2
Dawit <dawit@ginetta.net>
2019-09-02 15:31:55 +0200

    Commit put in dummy list within a static list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App" style={{ display: 'flex', flexDirection: 'row' }}>
+			<div style={{ display: 'flex', flexDirection: 'column' }}>
+				<div
+					style={{
+						display: 'inline-block',
+						width: '300px',
+						height: '100px',
+						backgroundColor: 'blue',
+						border: '1px solid darkgrey'
+					}}
+				/>
+				<div style={{ display: 'inline-block' }}>
+					<ul>
+						<li>First</li>
+						<li>Second</li>
+						<li>Third</li>
+					</ul>
+				</div>
+			</div>
+			<div style={{ display: 'flex', flexDirection: 'column' }}>
+				<div
+					style={{
+						display: 'inline-block',
+						width: '300px',
+						height: '100px',
+						backgroundColor: 'purple',
+						border: '1px solid darkgrey'
+					}}
+				/>
+				<div style={{ display: 'inline-block' }}>
+					<ul>
+						<li>First</li>
+						<li>Second</li>
+						<li>Third</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
