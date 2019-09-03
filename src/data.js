1cfcd834d11dc4c0351637c4e1df58d284422331
Dawit <dawit@ginetta.net>
2019-09-03 13:37:05 +0200

    Set up a directory called components and create a component called List.js

---
+++ src/data.js
@@ -0,0 +1,11 @@
+const movies = [
+	'Gone with the wind',
+	'Scent of a woman',
+	'The three musketeers',
+	"Devil's Advocate",
+	'Sweet November'
+];
+
+// make an array for characters as well
+
+const films = [ 'Keanu Reeves', 'Lost his name', 'Al Pacino', 'Leonardo De Caprio', 'Reese Witherspoon' ];
