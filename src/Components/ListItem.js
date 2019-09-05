3919803fa3636b5b226ed497e917b49abbb3e770
Dawit <dawit@ginetta.net>
2019-09-05 09:19:03 +0200

    Comment on what props.element does and how it's passed from the parent element

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
 const ListItem = (props) => {
-	// console.log(props);
-
+	// within the div, element is now passed via props from the parent List, which had the individual element items in the movies array populated (line 9, on the List.js file)
 	return <div>{props.element}</div>;
 };
 
 export default ListItem;
-
-// movieProp={movieProp} characterProp={characterProp}
