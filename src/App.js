5f30d220c5f56797ab624564116c328494c9e409
Dawit <dawit@ginetta.net>
2019-09-05 09:43:17 +0200

    Detailed notes on what the conditionals and the callback functions achieve

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
-// import './App.css';
 import './Components/List.css';
-// import the data
-import { movies, characters } from './data';
+import { movies, characters } from './data'; // imports the data from the data.js file, using named imports (in this case the variables movies and characters) instead of default component import
 import List from './Components/List';
-import ListItem from './Components/ListItem';
 
 function App(props) {
 	// define a new State and store it a variable called showMovies, and initialize this to be true
@@ -12,14 +9,13 @@ function App(props) {
 
 	// const listItems = [];
 	const handleShowMovies = () => {
-		// listItems.map((movie, i) => <li key={i}>{movie}</li>);
+		// initialise the showMovies variable to true, meaning when user clicks at the movies button only movies are going to be shown
 		setShowMovies(true);
-		// !setShowMovies(false);
 	};
 
 	const handleShowCharacters = () => {
+		// initialise the showMovies variable to false, meaning when user clicks at the movies button characters are not going to be shown
 		setShowMovies(false);
-		// setListMovies(false);
 	};
 
 	return (
@@ -39,7 +35,7 @@ function App(props) {
 				>
 					Movies
 				</button>
-
+				{/* the List component is now being rendered. The code statement of the prop named listItems is declared that it can have its state set to either movies or characters  */}
 				<List listItems={showMovies ? movies : characters} />
 			</div>
 			<div style={{ display: 'flex', flexDirection: 'column', marginTop: '50px' }}>
@@ -51,6 +47,7 @@ function App(props) {
 						backgroundColor: 'turquoise',
 						border: '1px solid darkgrey'
 					}}
+					// onClick event, the the handleShowCharacters function (defined above) becomes triggered, which will then follow on to go to the previous states above
 					onClick={handleShowCharacters}
 				>
 					Characters in the Movies
